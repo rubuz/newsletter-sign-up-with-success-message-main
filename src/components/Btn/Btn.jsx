cf95dd72b5ccb7e58b3377afa9592d521d2b12f8
rubuz <rubuz23@gmail.com>
2023-07-25 13:29:55 +0200

    static stayling done

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./btn.css";
 
 const Btn = () => {
-  return <button>TEXT</button>;
+  return <button className="btn">TEXT</button>;
 };
 
 export default Btn;
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,9 +3,13 @@ import "./input.css";
 
 const Input = () => {
   return (
-    <div>
-      <p className="input__text">Email address</p>
-      <input type="email" name="" id="" placeholder="email@company.com" />
+    <div className="input__container">
+      <div className="input__text-container">
+        <p className="input__text">Email address</p>
+        <p className="input__warning">Valid email required</p>
+      </div>
+
+      <input type="email" name="" id="input" placeholder="email@company.com" />
     </div>
   );
 };
